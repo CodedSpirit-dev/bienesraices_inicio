5c4235b2c248207dc894f3cb99354118e4831404
HalfSeraphic <VIradVT@gmail.com>
2023-01-31 19:38:27 -0600

    creando validaciones pt1

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,6 +3,73 @@
     require '../../includes/config/database.php';
     $db = conectardb();
 
+    //Arreglo con mensajes de errores
+    $errores = [];
+
+    //Ejecutar el codigo despues de que el usuario envia el formulario
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        /*echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";*/
+
+        $titulo = $_POST['titutlo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedores_id = $_POST['vendedor'];
+
+        if(!$titulo){
+            $errores[] = "Debes añadir un titulo";
+        }
+        if(!$precio){
+            $errores[] = "Debes añadir el precio";
+        }
+        if(!$descripcion){
+            $errores[] = "Debes añadir una descripcion";
+        }
+        if(!$habitaciones){
+            $errores[] = "Debes añadir el numero de habitaciones";
+        }
+        if(!$wc){
+            $errores[] = "Debes añadir el numero de baños";
+        }
+        if(!$estacionamiento){
+            $errores[] = "Debes añadir el numero de estacionamientos";
+        }
+        if(!$vendedores_id){
+            $errores[] = "Debes añadir el vendedor";
+        }
+
+        
+        echo "<pre>";
+        var_dump($errores);
+        echo "</pre>";
+
+    exit;
+
+
+        //Insertar en la base de datos
+
+        $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_id) VALUES
+                                           ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedores_id')";
+
+        //echo $query;
+
+        //Agregarlo a la base de datos
+        $resultado = mysqli_query($db, $query);
+
+        if($resultado){
+            echo "Insertado Correctamente";
+        }
+
+
+
+    }
+    
+
     require '../../includes/funciones.php';
     incluirTemplate('header', $inicio = false);
 ?>
@@ -12,22 +79,22 @@
         <h1>Crear</h1>
 
         <a href="/admin/index.php" class="boton boton-verde">Volver</a>
-        <form action="" class="formulario">
+        <form method="POST" action="/admin/propiedades/crear.php" class="formulario">
 
         <fieldset>
             <legend>Informacion General</legend>
 
             <label for="titulo">Titulo:</label>
-            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+            <input type="text" id="titulo" name="titutlo" placeholder="Titulo Propiedad">
 
             <label for="precio">Precio:</label>
-            <input type="number" id="precio" placeholder="Precio Propiedad">
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
 
             <label for="imagen">Imagen:</label>
             <input type="file" id="precio" accept="image/jpeg, image/png">
 
             <label for="descripcion">Descripcion</label>
-            <textarea id="descripcion"></textarea>
+            <textarea id="descripcion" name="descripcion"></textarea>
         </fieldset>
 
         <fieldset>
@@ -35,13 +102,13 @@
             <legend>Informacion Propiedad</legend>
 
             <label for="habitaciones">Habitaciones:</label>
-            <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
 
             <label for="wc">WC:</label>
-            <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
 
             <label for="estacionamiento">Estacionamiento:</label>
-            <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">
 
 
         </fieldset>
