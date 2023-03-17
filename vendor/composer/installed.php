cec3a924a7730046e2983cb5b53815f7501817b5
HalfSeraphic <96750594+HalfSeraphic@users.noreply.github.com>
2023-03-17 15:42:00 -0600

    POO integration

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'victor/bienesraices_inicio',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2d0e54d9036282fdfccd1a3d56d5e5ab62f1ce40',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'victor/bienesraices_inicio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2d0e54d9036282fdfccd1a3d56d5e5ab62f1ce40',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
